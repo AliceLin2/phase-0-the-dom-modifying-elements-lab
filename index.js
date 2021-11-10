43fe175a2fcfdfd6ac22c37a1b615a55689219f1
shu Lin <bbqlinshu@gmail.com>
2021-11-10 00:11:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element=document.querySelector('main')
+element.remove();
+const newHeader=document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML="<p>is the champion</p>";
